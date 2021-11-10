14c4ca178f82c03b3e32093175660fee3f436ada
Kotkoa <Kotkoa@gmail.com>
2021-11-10 22:05:29 +0300

    add script that stars on sh... page

---
+++ contentScript.js
@@ -0,0 +1,5 @@
+if (window.location.href.includes('https://submit.shutterstock.com/earnings/top-performers?language=en')){
+
+    document.getElementById('top-performers-headline').innerText = 'This is new headline!'
+    
+}
